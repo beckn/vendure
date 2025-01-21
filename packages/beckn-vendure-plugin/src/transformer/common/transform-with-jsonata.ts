b5a3dfb91b4a642f8783ec4ec20f51120963cfc3
shreyvishal <110912421+shreyvishal@users.noreply.github.com>
2025-01-21 15:30:47 +0530

    Update transform-with-jsonata.ts

--- packages/beckn-vendure-plugin/src/transformer/common/transform-with-jsonata.ts
+++ packages/beckn-vendure-plugin/src/transformer/common/transform-with-jsonata.ts
@@ -5,6 +5,7 @@ import jsonata from 'jsonata';
 
 export async function transformWithJSONata(data: any, jsonataFilename: string) {
     const expression = jsonata(await readFile(jsonataFilename, 'utf-8'));
-    console.log("Transforming with jsonataFilename===>",JSON.stringify(data));
-    return await expression.evaluate(data);
+    console.log(`Transforming with ${jsonataFilename}===>`, JSON.stringify(data),\n\n);
+    const evaluatedData = await expression.evaluate(data);
+    return evaluatedData
 }
